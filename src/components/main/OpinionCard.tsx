8af0ade3ff0489861c3d8825c0614e257a577e31
Adrian Rudnicki <adi2602@wp.pl>
2024-01-09 18:57:36 +0100

    card for option fix

--- src/components/main/OpinionCard.tsx
+++ src/components/main/OpinionCard.tsx
@@ -4,7 +4,7 @@ import { TOpinionCard } from "./Opinion";
 
 function OpinionCard({ name, opinion }: TOpinionCard) {
   return (
-    <Card className="rounded-none border-0 shadow-xl">
+    <Card className="rounded-none border-0 shadow-xl min-h-full">
       <CardHeader>
         <CardTitle>
           <div className="flex w-full justify-center p-4">
