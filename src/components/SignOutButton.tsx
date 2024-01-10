882c2a386ad6a5a95c770339e0290a73f09e8673
Adrian Rudnicki <adi2602@wp.pl>
2024-01-11 00:32:43 +0100

    logout button fix

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -16,6 +16,7 @@ function SignOutButton() {
   const handleSignOut = async () => {
     setStatus("loading");
     await signOut({ redirect: false });
+    setStatus("idle");
     router.push("/auth/signin");
   };
 
